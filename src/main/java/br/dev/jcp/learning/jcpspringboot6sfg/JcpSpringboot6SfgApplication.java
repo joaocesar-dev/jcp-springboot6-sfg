afc58159e25561074ec451e45540cf9257a4d9ab
Joao Cesar Pereira <joao.cesar@jcp.dev.br>
2024-05-18 10:21:38 -0300

    Initial Commit

---
+++ src/main/java/br/dev/jcp/learning/jcpspringboot6sfg/JcpSpringboot6SfgApplication.java
@@ -0,0 +1,13 @@
+package br.dev.jcp.learning.jcpspringboot6sfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcpSpringboot6SfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcpSpringboot6SfgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/jcp/learning/jcpspringboot6sfg/JcpSpringboot6SfgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.jcp.learning.jcpspringboot6sfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcpSpringboot6SfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
